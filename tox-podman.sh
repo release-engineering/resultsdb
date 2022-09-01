0c72e50b54b2b37397fc4cc1990b811ae6bf5d25
Lukas Holecek <hluk@email.cz>
2022-09-01 08:46:23 +0200

    Use poetry to manage dependencies
    Drops spec file and setup.py.

--- tox-podman.sh
+++ tox-podman.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 set -euo pipefail
 
+podman_service_pid=""
+cleanup() {
+    containers=$(podman ps --filter 'name=resultsdb-postgres.*' --format '{{.Names}}')
+    podman rm --force resultsdb-postgres $containers
+    if [[ -n "$podman_service_pid" ]]; then
+        kill "$podman_service_pid"
+    fi
+}
+
+cleanup
+trap cleanup QUIT TERM INT HUP EXIT
+
 export DOCKER_HOST=unix:///run/user/$UID/resultsdb-podman.sock
 podman system service --time=0 "$DOCKER_HOST" &
-trap "kill $!" QUIT TERM INT HUP EXIT
+podman_service_pid=$!
 sleep 1
 
 tox "$@"
