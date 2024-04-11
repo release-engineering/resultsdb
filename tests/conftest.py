be96a422c0a1eeda4089043005f1a14481e0166d
Lukas Holecek <hluk@email.cz>
2024-04-11 16:44:15 +0200

    Check code coverage

--- tests/conftest.py
+++ tests/conftest.py
@@ -26,33 +26,6 @@ def app(mock_oidc):
         yield app
 
 
-def pytest_addoption(parser):
-    """
-    Add an option to the py.test parser to detect when the functional tests
-    should be detected and run
-    """
-
-    parser.addoption(
-        "-F",
-        "--functional",
-        action="store_true",
-        default=False,
-        help="Add functional tests",
-    )
-
-
-def pytest_ignore_collect(collection_path, config):
-    """Prevents collection of any files named functest* to speed up non
-    integration tests"""
-    if collection_path.match("*functest*"):
-        try:
-            is_functional = config.getvalue("functional")
-        except KeyError:
-            return True
-
-        return not is_functional
-
-
 def pytest_configure(config):
     """Called after command line options have been parsed and all plugins and
     initial conftest files been loaded."""
